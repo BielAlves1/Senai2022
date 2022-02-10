27eaddf0e5c4df9e597d537f03af64e4b2508c90
BielAlves1 <gabriel2002.alves1010@gmail.com>
2022-02-10 11:23:50 -0300

    SOP

---
+++ SOP/SO/arquivos.bat
@@ -0,0 +1,16 @@
+echo off
+echo "oi" > htmllocal\assets\img1.png
+echo "oi" > htmllocal\assets\img2.png
+echo "oi" > htmllocal\assets\img3.png
+echo "oi" > htmllocal\assets\img4.png
+echo "oi" > htmllocal\pages\page1.html
+echo "oi" > htmllocal\pages\page2.html
+echo "oi" > htmllocal\pages\page3.html
+echo "oi" > htmllocal\styles\style1.css
+echo "oi" > htmllocal\styles\style2.css
+echo "oi" > htmllocal\styles\style3.css
+echo "oi" > htmllocal\scripts\script1.js
+echo "oi" > htmllocal\scripts\script2.js
+echo "oi" > htmllocal\scripts\script3.js
+dir /s
+Tree
---
+++ SOP/lote/pastas.bat
@@ -0,0 +1,16 @@
+echo Criando a pasta "lol"
+md lol
+echo Acessando a pasta "lol"
+cd lol
+echo Criando as subpastas "sup, adc, mid, jg e top"
+md sup
+md adc
+md mid
+md jg
+md top
+echo Saindo da pasta "lol"
+cd ..
+echo mostrando o que foi feito
+dir
+Tree
+
