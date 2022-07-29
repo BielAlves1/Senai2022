c5b442c6103ac58f0901b74ad4e65430ec243fcd
BielAlves1 <gabriel2002.alves1010@gmail.com>
2022-07-29 00:21:41 -0300

    Lista Completa

--- 2DES/FrontEnd/Aula 4/list.js
+++ 2DES/FrontEnd/Aula 4/list.js
@@ -1,6 +1,7 @@
 //Exercício 1
 var btPlaca = document.querySelector("#btPlaca");
 btPlaca.addEventListener("click", validarPlaca);
+
 function validarPlaca(placa){
     placa = document.querySelector("#placa");
     let validando = false;
@@ -20,6 +21,7 @@ function validarPlaca(placa){
 //Exercício 2
 var btCPF = document.querySelector("#btCPF");
 btCPF.addEventListener("click", validarCPF);
+
 function validarCPF(cpf){
     cpf = document.querySelector("#cpf");
     let textArea = document.querySelector("#cpfRes");
@@ -46,6 +48,7 @@ function validarCPF(cpf){
 //Exercício 3
 var btTel = document.querySelector("#btTel");
 btTel.addEventListener("click", gerarTel);
+
 function gerarTel(DDD, quant){
     quant = document.querySelector("#quantTel");
     let textArea = document.querySelector("#telefones");
@@ -53,10 +56,50 @@ function gerarTel(DDD, quant){
 
     let i = 0;
     while(i < quant.value){
-        let tel = `(${DDD})${Math.floor(Math.random() * (99999 - 11111 + 1)) + 111111}-${Math.floor(Math.random() * (9999 - 1111 + 1)) + 1111}          `;
+        let tel = `(${DDD})${Math.floor(Math.random() * (99999 - 11111 + 1)) + 111111}-${Math.floor(Math.random() * (9999 - 1111 + 1)) + 1111}\n`;
         textArea.innerHTML += tel;
         i++
     }
 }
 
 //Exercício 4
+var btGCPF = document.querySelector("#btGCPF");
+btGCPF.addEventListener("click", gerarCPF);
+
+function gerarCPF(){
+
+    const cpf1 = document.querySelector("#cpfs");
+    const cpf2 = document.querySelector("#quantCPF");
+
+    for(let i = 0; i < parseInt(cpf2.value); i++){
+          cpf1.value += `${CPFgera()}\n`;
+    }
+
+    function CPFgera(){
+        const num1 = ("" + Math.floor(Math.random() * 999)).padStart(3,0);
+        const num2 = ("" + Math.floor(Math.random() * 999)).padStart(3,0);
+        const num3 = ("" + Math.floor(Math.random() * 999)).padStart(3,0);
+        const dig1 = dig(num1, num2, num3);
+        const dig2 = dig(num1, num2, num3, dig1);
+        return `${num1}.${num2}.${num3}-${dig1}${dig2}`
+    }
+
+    function dig(n1, n2, n3, n4){
+        const nums = n1.split("").concat(n2.split(""), n3.split(""));
+        if(n4 !== undefined){
+              nums[9] = n4;
+        }
+    
+        let x = 0;
+        for(let i = (n4 !== undefined ? 11 : 10), j = 0; i >= 2; i--, j++){
+              x += parseInt(nums[j]) * i;
+        }
+    
+        const y = x % 11;
+        return y < 2 ? 0 : 11 - y;
+    }
+}
+
+
+
+
