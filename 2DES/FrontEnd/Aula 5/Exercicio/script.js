523cee64c4fabaf0d5c7fee37f076a5ec4712f7a
BielAlves1 <gabriel2002.alves1010@gmail.com>
2022-08-03 04:05:41 -0300

    Ecercício

---
+++ 2DES/FrontEnd/Aula 5/Exercicio/script.js
@@ -0,0 +1,18 @@
+function cadastrar(){
+    let nome = document.querySelector("#nome");
+    let cargo = document.querySelector("#cargo");
+    let salario = document.querySelector("#salario");
+
+    let linha = document.createElement("tr");
+    let colun = document.createElement("td");
+    colun.append(nome.value);
+    let colun2 = document.createElement("td");
+    colun2.append(cargo.value);
+    let colun3 = document.createElement("td");
+    colun3.append("R$ ", salario.value);
+
+    document.querySelector("#tb").appendChild(linha);
+    linha.appendChild(colun);
+    linha.appendChild(colun2);
+    linha.appendChild(colun3);
+}
