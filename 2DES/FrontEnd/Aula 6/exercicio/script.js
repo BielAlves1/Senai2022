770e5b7d361fc32674996d534a3b2eb8e7422bf4
BielAlves1 <gabriel2002.alves1010@gmail.com>
2022-08-10 11:23:59 -0300

    Aula 7

--- 2DES/FrontEnd/Aula 6/exercicio/script.js
+++ 2DES/FrontEnd/Aula 6/exercicio/script.js
@@ -46,26 +46,47 @@ var funcionario = [
 			"nível":3
 		},
 		"autorizado":true
-	}
-]
+	},
+];
 
 var infoFuncionario = document.querySelector(".info-funcionario");
 
 
-function carregar(){
+function load(){
     funcionario.forEach(info => {
         let novoFuncionario = infoFuncionario.cloneNode(true);
+		let btRemover = novoFuncionario.querySelector(".btRemover");
+		let adm = novoFuncionario.querySelector(".adm");
+		adm.style = "color: rgb(9, 212, 9);"
+		let leitor = novoFuncionario.querySelector(".leitor");
+		leitor.style = "color: aqua;";
 
         novoFuncionario.classList.remove("model");
         
         novoFuncionario.querySelector("#nome-funcionario").innerHTML = info.funcionario;
-        novoFuncionario.querySelector("#cargo").appendChild("#cargo-funcionario").value = info.cargo.nome;
-        novoFuncionario.querySelector("#cargo").appendChild("#nivel-funcionario").value = info.cargo.nivel;
-        novoFuncionario.querySelector("#autoriza").value = info.autorizado;
+        novoFuncionario.querySelector("#cargo").innerHTML = info.cargo.nome;
+        novoFuncionario.querySelector("#nivel").innerHTML = info.cargo.nível;
+		if(info.autorizado == true){
+			novoFuncionario.querySelector("#autoriza").innerHTML = "Autorizado(a)";
+		}else{
+			novoFuncionario.querySelector("#autoriza").innerHTML = "Não Autorizado(a)";
+		}
+
         document.querySelector(".funcionario").appendChild(novoFuncionario);
+
+		if(info.cargo.nível != 3){
+			btRemover.classList.remove("model");
+		}
+		if((info.cargo.nome) == 'Analista' || (info.cargo.nome) == 'Desenvolvedor'){
+			if(info.cargo.nível == 3){
+				adm.classList.remove("model");
+			}else {
+				leitor.classList.remove("model");
+			}
+		}
     });
 }
 
-function removerItem(e) {
+function removerFuncionario(e) {
     e.parentNode.remove();
 }
